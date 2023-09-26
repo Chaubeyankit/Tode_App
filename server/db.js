6e0dd20b53a3ded7fa42a2e1f35cd5f59e7bf18f
Ankit chaubey <90557067+Chaubeyankit@users.noreply.github.com>
2023-09-27 01:37:33 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURL = "mongodb://localhost:27017/i-notebook";
+const mongoURL = "mongodb://localhost:27017/todo_list";
 const connectToMongo = () => {
       mongoose.connect(mongoURL, () => {
             console.log("Connectd to db")
